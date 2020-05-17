cd589208086d0e64c93a40cd19e388319e8ba4ff
Igor V. Trofimov <itrofimov@gmail.com>
2020-05-18 01:42:56 +0300

    windows make

--- demo-windows.cmd
+++ demo-windows.cmd
@@ -1,27 +1,50 @@
 @echo off
 
-if not exist distance.exe (
+SETLOCAL
+SET SIZE_DEP=80
+SET SIZE_ASSOC=20
+SET TRAIN_FN=parus_first_10m_lines.conll
+SET COL_EMB=3
+SET COL_CTX_D=3
+SET USE_DEPREL=1
+SET MODEL_FN=vectors.bin
+SET VOC_M=main.vocab
+SET VOC_P=proper.vocab
+SET VOC_D=ctx_dep.vocab
+SET VOC_A=ctx_assoc.vocab
+SET THREADS=8
+
+
+if not exist conll2vec.exe (
   echo.
   echo "MAKING BINARIES"
   nmake -f makefile.msvc
+  copy ./data/stopwords.assoc .
 )
 
-if not exist text8 (
+if not exist %TRAIN_FN% (
   echo.
-  echo DOWNLOADING TRAINING TEXT DATA 
+  echo TRAINSET EXTRACTION AND FITTING
   echo   please wait...
-  cscript //Nologo helpers/demo.cmd.wget.js http://mattmahoney.net/dc/text8.zip text8.zip
-  cscript //Nologo helpers/demo.cmd.unzip.vbs %cd% %cd%\text8.zip
+  cscript //Nologo helpers/demo.cmd.unzip.vbs %cd% %cd%\data\parus_first_10m_lines.conll.zip
+  rename parus_first_10m_lines.conll pre_fit.conll
+  conll2vec -task fit -fit_input pre_fit.conll -train %TRAIN_FN%
+  del /f pre_fit.conll
 )
 
 echo.
-echo BUILDING VOCABULARY
-build_dict -train text8 -min-count 200 -save-vocab text8.vocab_200
+echo BUILDING VOCABULARIES
+conll2vec -task vocab -train %TRAIN_FN% -col_emb %COL_EMB% -col_ctx_d %COL_CTX_D% -vocab_m %VOC_M% -vocab_p %VOC_P% -vocab_d %VOC_D% -vocab_a %VOC_A% -min-count_m 70 -min-count_p 100 -min-count_d 20 -min-count_a 20 -use_deprel %USE_DEPREL%
 
 echo.
-echo TRAINING EMBEDDINGS
-cbow -train text8 -words-vocab text8.vocab_200 -output vectors.bin -size 200 -window 2 -iter 5 -threads 8 -optimization ns -negative 5
+echo TRAINING EMBEDDINGS -- MAIN
+conll2vec -task train -train %TRAIN_FN% -col_emb %COL_EMB% -col_ctx_d %COL_CTX_D% -use_deprel %USE_DEPREL% -vocab_m %VOC_M% -backup backup.data -vocab_d %VOC_D% -vocab_a %VOC_A% -model %MODEL_FN% -size_d %SIZE_DEP% -size_a %SIZE_ASSOC% -negative 5 -iter 10 -threads %THREADS%
+
+echo.
+echo TRAINING EMBEDDINGS -- PROPER
+conll2vec -task train -train %TRAIN_FN% -col_emb %COL_EMB% -col_ctx_d %COL_CTX_D% -use_deprel %USE_DEPREL% -vocab_p %VOC_P% -restore backup.data -vocab_d %VOC_D% -vocab_a %VOC_A% -model %MODEL_FN% -size_d %SIZE_DEP% -size_a %SIZE_ASSOC% -negative 5 -iter 5 -threads %THREADS%
 
 echo.
 echo RUN SIMILARITY METER
-distance vectors.bin
+conll2vec -task sim -model %MODEL_FN% -model_fmt bin -size_d %SIZE_DEP% -size_a %SIZE_ASSOC%
+
