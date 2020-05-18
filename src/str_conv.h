901377ab5a03bc8b03f62d602ba7b521007a7c7e
Igor V. Trofimov <itrofimov@gmail.com>
2020-05-18 19:06:17 +0300

    msvc utf conv fix

--- src/str_conv.h
+++ src/str_conv.h
@@ -2,8 +2,12 @@
 #define STR_CONV_H_
 
 #include <string>
-#include <bits/locale_conv.h>        // определение std::wstring_convert (todo: странность gcc?)
-#include <codecvt>
+#ifdef _MSC_VER
+  #include <codecvt>
+#else
+  #include <bits/locale_conv.h>        // определение std::wstring_convert (todo: странность gcc?)
+  #include <codecvt>
+#endif
 
 class StrConv
 {
@@ -11,13 +15,25 @@ public:
   // конвертеры кодировок UTF-8  <--> UTF-32
   static std::string To_UTF8(const std::u32string &s)
   {
-      std::wstring_convert<std::codecvt_utf8<char32_t>, char32_t> conv;
-      return conv.to_bytes(s);
+      #if _MSC_VER >= 1900 && _MSC_VER < 2000
+        static std::wstring_convert<std::codecvt_utf8<__int32>, __int32> conv;
+        auto p = reinterpret_cast<const int32_t *>(s.data());
+        return conv.to_bytes(p, p + s.size());
+      #else
+        static std::wstring_convert<std::codecvt_utf8<char32_t>, char32_t> conv;
+        return conv.to_bytes(s);
+      #endif
   }
   static std::u32string To_UTF32(const std::string &s)
   {
-      std::wstring_convert<std::codecvt_utf8<char32_t>, char32_t> conv;
-      return conv.from_bytes(s);
+      #if _MSC_VER >= 1900 && _MSC_VER < 2000
+        static std::wstring_convert<std::codecvt_utf8<__int32>, __int32> conv;
+        auto r = conv.from_bytes(s);
+        return reinterpret_cast<const char32_t *>(r.data());
+      #else
+        static std::wstring_convert<std::codecvt_utf8<char32_t>, char32_t> conv;
+        return conv.from_bytes(s);
+      #endif
   }
 };
 
